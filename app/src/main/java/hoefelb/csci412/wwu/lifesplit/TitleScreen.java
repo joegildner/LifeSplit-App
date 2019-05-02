d066dc91691c543d5fb335f9037808d7f3daf9f9
hoefelb <hoefelb@wwu.edu>
2019-05-02 14:32:18 -0700

    Add android studio files

---
+++ app/src/main/java/hoefelb/csci412/wwu/lifesplit/TitleScreen.java
@@ -0,0 +1,28 @@
+package hoefelb.csci412.wwu.lifesplit;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+
+        final Button titleButton = findViewById(R.id.title_button);
+        titleButton.setBackgroundColor(Color.TRANSPARENT);
+        titleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(TitleScreen.this, TimingScreen.class));
+                findViewById(R.id.text_view).setVisibility(View.INVISIBLE);
+                //findViewById(R.id.title_image).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+}
